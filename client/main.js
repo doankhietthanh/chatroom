5285f8c5a859d6994badd083060718847246e382
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2021-11-28 12:15:59 +0700

    change mainjs

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 const socket = io();
 const $ = document.querySelector.bind(document);
 
+const username = prompt("Name:");
+
 const text = $("#text");
 const btn_send = $("#send");
 const list = $("#chatbox ul");
@@ -9,20 +11,24 @@ const chatList = [];
 const render = () => {
   let html = "";
   chatList.forEach((i) => {
-    html += `<li>${i.text}</li>`;
+    html += `<li>${i.username}: ${i.text}</li>`;
   });
   list.innerHTML = html;
 };
+
 render();
 const addText = (text) => {
   chatList.push(text);
   render();
 };
+
 btn_send.addEventListener("click", (e) => {
   e.preventDefault();
 
-  addText({ text: text.value });
-  socket.emit("msg", { text: text.value });
+  addText({ username: username, text: text.value });
+  socket.emit("msg", { username: username, text: text.value });
+
+  text.value = "";
 });
 
 socket.on("msg-end", (d) => {
